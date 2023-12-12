981c6fd9ad9c0eecbd898a8d3a39f3236e5aebb3
Marc Mognol <8171300+marcmognol@users.noreply.github.com>
2023-12-12 11:13:53 +0000

    Add

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,9 +1,3 @@
 module.exports = {
-    hostRules: [
-      {
-        hostType: 'terraform-module',
-        matchHost: 'app.terraform.io',
-        token: process.env.RENOVATE_TFE_TOKEN,
-      },
-    ],
+    autodiscover: true
   };
