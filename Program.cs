2db4141dd268f87335cea6a896a90bca9c7b6e95
Marc Mognol <8171300+marcmognol@users.noreply.github.com>
2023-12-12 10:42:12 +0000

    Add

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using renovate_issue_26237;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
